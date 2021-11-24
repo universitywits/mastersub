1329259bfd96b7ca0e497b3779837964274202db
Ashik Muhammed <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-11-24 18:38:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,6 +47,10 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌�
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+COMMAND_HAND_LER = "/"
+SUDO_USERS = "1961162381"
+USE_AS_BOT = True
+TG_MAX_SELECT_LEN = "1000"
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
